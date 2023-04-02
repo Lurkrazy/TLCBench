aac762cec42ee39ef11769b7b975ebffb416ad4b
Chendi <chendi.li@outlook.com>
2023-04-01 23:56:42 -0600

    new run scrips

--- run_resnet_50.sh
+++ run_resnet_50.sh
@@ -3,25 +3,26 @@
 #SBATCH --account=soc-gpu-np
 #SBATCH --partition=soc-gpu-np
 #SBATCH --job-name=resnet_50
-#SBATCH --output=resnet_50.log
 #SBATCH --nodes=1
-#SBATCH --ntasks=28
-#SBATCH --mem=0
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=64GB
 #SBATCH --gres=gpu:a100:1
+#SBATCH --output=bert.log
 
 # load appropriate modules
 source /uufs/chpc.utah.edu/common/home/u0940848/load_tvm_v09.sh
-source exportTVM.sh                                                                                                                                                                                                               
+source exportTVM.sh
 module list
 echo $PYTHONPATH
 OUT_DIR=$(pwd)/PrintOut
 echo $OUT_DIR
 mkdir -p $OUT_DIR
 FILE_PATH=/uufs/chpc.utah.edu/common/home/u0940848/chendi.li/git/TLCBench
-
 echo $FILE_PATH
 cd $FILE_PATH
 
+# Run BERT network
 echo "==== Start Execution =====" 
-python3 tune_autoscheduler.py --network resnet_50 --target "cuda -model=a100" 2>&1 | tee resnet_50.log
+python3 tune_autoscheduler.py --network resnet_50 --target "cuda -model=a100" > $OUT_DIR/resnet_50_output.txt 2>&1
 echo "==== Done ======"
--- run_vgg.sh
+++ run_vgg.sh
@@ -3,25 +3,26 @@
 #SBATCH --account=soc-gpu-np
 #SBATCH --partition=soc-gpu-np
 #SBATCH --job-name=vgg
-#SBATCH --output=vgg.log
 #SBATCH --nodes=1
-#SBATCH --ntasks=28
-#SBATCH --mem=0
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=64GB
 #SBATCH --gres=gpu:a100:1
+#SBATCH --output=bert.log
 
 # load appropriate modules
 source /uufs/chpc.utah.edu/common/home/u0940848/load_tvm_v09.sh
-source exportTVM.sh                                                                                                                                                                                                               
+source exportTVM.sh
 module list
 echo $PYTHONPATH
 OUT_DIR=$(pwd)/PrintOut
 echo $OUT_DIR
 mkdir -p $OUT_DIR
 FILE_PATH=/uufs/chpc.utah.edu/common/home/u0940848/chendi.li/git/TLCBench
-
 echo $FILE_PATH
 cd $FILE_PATH
 
+# Run BERT network
 echo "==== Start Execution =====" 
-python3 tune_autoscheduler.py --network vgg --target "cuda -model=a100" 2>&1 | tee vgg.log
+python3 tune_autoscheduler.py --network vgg --target "cuda -model=a100" > $OUT_DIR/vgg_output.txt 2>&1
 echo "==== Done ======"
